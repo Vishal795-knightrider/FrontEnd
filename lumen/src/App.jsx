688a2ee97edf2907670b8e9b30fae0d1d02c0390
Vishal795-knightrider <vk3293801@gmail.com>
2026-02-10 21:18:00 +0530

    add lumen app

---
+++ lumen/src/App.jsx
@@ -0,0 +1,31 @@
+import { UserCard } from './UserCard';
+import { Wrapper } from './Wrapper';
+import './App.css'
+
+export default function App() 
+{  const users=[
+  {name:"Virat Kohli",
+    role:"BatsMan",
+    country:"India"
+  },
+
+  {
+    name:"Ms. Dhoni",
+    role:"WK-Batsman",
+    country:"India"
+  }
+];
+
+  return (
+    <>
+
+    <h1>User DashBoard</h1>
+    <Wrapper title="User list">     
+    {
+    users.map((data)=>(<UserCard data={data}/>))
+    }
+    </Wrapper>
+  
+    </>
+  )
+}
---
+++ lumen/src/UserCard.jsx
@@ -0,0 +1,11 @@
+export function UserCard({data}){
+    return(
+
+        <div>
+            <h4>Name:{data.name}</h4>
+            <p>roll:{data.role}</p>
+            <p>Country:{data.country}</p>
+        </div>
+
+    );
+}
