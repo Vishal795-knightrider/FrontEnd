9865fcd031d293da6ad95eff01a4c25a2e68e858
Vishal795-knightrider <vk3293801@gmail.com>
2026-03-19 20:32:26 +0530

    added new project

--- practise/Promise.js
+++ practise/Promise.js
@@ -13,7 +13,7 @@ else{
 
 //consuming promise
 
-fetch("https://jsonplaceholder.typicode.com/users")       //isme url ayega jis website me hm jayenge  (yeh promise return karega)
+fetch("https://jsonplaceholder.typicode.com/users")    //isse hm do end point ke bich connection establish karenge ,to uske beech kuch time lageag usse hame solve krna hai to agar connection fail hua to error agar pass hua to then //isme url ayega jis website me hm jayenge  (yeh promise return karega)
 .then(response=>response.json())
 .then(data=>data.map((user)=>console.log(user.name,user.id)))
 .catch(error=>console.log(error))
